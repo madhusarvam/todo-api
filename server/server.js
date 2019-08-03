9e9d1d44cb7373c262ef88346e9d2d22206d7029
prathap <prathap@steedserv.com>
2019-08-03 10:44:10 +0530

    Todo Post

---
+++ server/server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+
+var {mongoose} =  require('./db/mongoose');
+var {Todo} =  require('./models/todo');
+var {User} =  require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+  var todo = new Todo({
+    text  : req.body.text
+  });
+  todo.save().then((doc)=>{
+    res.send(doc);
+  },(e)=>{
+    res.status(400).send(e);
+  })
+
+});
+
+
+app.post('/users',(req,res)=>{
+
+  var user = new User({
+    email : req.body.email
+  });
+  user.save().then((doc)=>{
+    res.send(doc);
+  },(e)=>{
+     res.status(400).send(e);
+  })
+
+})
+
+
+app.listen(3001,()=>{
+  console.log('started on port 3001')
+});

d997b07e55d948d56807a82966b9810e34966958
prathap <prathap@steedserv.com>
2019-08-03 13:39:52 +0530

    v2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,6 @@
-const mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://madhusarvam:Amma@1191@ds01316.mlab.com:1316/todos',{ useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
-module.exports = {
-  mongoose : mongoose
-}
+module.exports = {mongoose};

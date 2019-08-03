7420da4104c14a3e078e8a8309021673832a3efd
prathap <prathap@steedserv.com>
2019-08-03 11:41:43 +0530

    GET todos

--- server/server.js
+++ server/server.js
@@ -1,41 +1,40 @@
 var express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
-var {mongoose} =  require('./db/mongoose');
-var {Todo} =  require('./models/todo');
-var {User} =  require('./models/user');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos',(req,res)=>{
+app.post('/todos', (req, res) => {
   var todo = new Todo({
-    text  : req.body.text
+    text: req.body.text
   });
-  todo.save().then((doc)=>{
-    res.send(doc);
-  },(e)=>{
-    res.status(400).send(e);
-  })
 
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-
-app.post('/users',(req,res)=>{
-
-  var user = new User({
-    email : req.body.email
-  });
-  user.save().then((doc)=>{
-    res.send(doc);
-  },(e)=>{
-     res.status(400).send(e);
+app.get('/todos',(req, res)=>{
+  Todo.find().then((doc)=>{
+    res.send({
+      doc
+    });
+  },(err)=>{
+     res.status(400).send({
+      err
+    });
   })
-
 })
 
-
-app.listen(3001,()=>{
-  console.log('started on port 3001')
+app.listen(3000, () => {
+  console.log('Started on port 3000');
 });
+
+module.exports = {app};
